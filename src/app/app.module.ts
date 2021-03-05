a144852f64af84c1425bc2aa3e08dcee45d82229
lumusitec <lumusitec@gmail.com>
2021-03-05 17:59:51 -0300

    header, navbar, footer - bootstrap test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { MainPageModule } from './main-page/main-page.module';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
